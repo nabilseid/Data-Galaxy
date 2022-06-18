92e61e73f42d408f4fe932654a6b95133a6cabea
Nabil Seid <nabeelseid@gmail.com>
2022-06-18 23:15:20 +0300

    Add binary-tree-nodes solution

---
+++ sql-nebulae/hackerRank-sql-prepare/advanced-select/binary-tree-nodes.sql
@@ -0,0 +1,53 @@
+/*
+You are given a table, BST, containing two columns: N and P, where N 
+represents the value of a node in Binary Tree, and P is the parent of N.
+
++------------------+
+| Column | Type    |
++------------------+
+| N      | Integer |
+| P      | Integer |
++------------------+
+
+Write a query to find the node type of Binary Tree ordered by the value of 
+the node. Output one of the following for each node:
+
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+
+Sample Input
+
++----------+
+| N | P    |
++----------+
+| 1 | 2    |
+| 3 | 2    |
+| 6 | 8    |
+| 9 | 8    |
+| 2 | 5    |
+| 8 | 5    |
+| 5 | null |
++----------+
+
+Sample Output
+
+1 Leaf
+2 Inner
+3 Leaf
+5 Root
+6 Leaf
+8 Inner
+9 Leaf
+*/
+
+-- Solution
+SELECT
+    n,
+    CASE
+        WHEN p IS NULL THEN 'Root'
+	WHEN n IN (SELECT p FROM BST) THEN 'Inner'
+        ELSE 'Leaf'
+    End
+FROM BST
+ORDER BY n;
