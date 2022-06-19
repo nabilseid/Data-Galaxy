61fe4bc1ee9c6382254a07b37341fc00334cd7a0
Nabil Seid <nabeelseid@gmail.com>
2022-06-19 12:34:09 +0300

    Add japan-population solution

---
+++ sql-nebulae/hackerRank-sql-prepare/aggregation/japan-population.sql
@@ -0,0 +1,23 @@
+/*
+Query the sum of the populations for all Japanese cities in CITY. 
+The COUNTRYCODE for Japan is JPN.
+
+Input Format
+
+The CITY table is described as follows:
+
++--------------------------+
+|Field       |TYPE         |
++--------------------------+
+|ID          |NUMBER       |
+|NAME        |VARCHAR2(17) |
+|COUNTRYCODE |VARCHAR2(3)  |
+|DISTRICT    |VARCHAR2(20) |
+|POPULATION  |NUMBER       |
++--------------------------+
+*/
+
+-- solution
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
