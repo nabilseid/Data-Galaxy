83529630b2a37316285319425d38e6c749ef8355
Nabil Seid <nabeelseid@gmail.com>
2022-06-20 17:30:04 +0300

    Add weather-obsercation-station-13 solution

---
+++ sql-nebulae/hackerRank-sql-prepare/aggregation/weather-observation-station-13.sql
@@ -0,0 +1,25 @@
+/*
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater 
+than 38.7880 and less than 137.2345 . Truncate your answer to  decimal places.
+
+Input Format
+
+The STATION table is described as follows:
+
++--------------------------+
+|Field       |TYPE         |
++--------------------------+
+|ID          |NUMBER       |
+|CITY        |VARCHAR2(21) |
+|STATE       |VARCHAR2(2)  |
+|LAT_N       |NUMBER       |
+|LONG_W      |NUMBER       |
++--------------------------+
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+ */
+
+-- solution
+SELECT TRUNCATE(SUM(LAT_N), 4)
+FROM STATION
+WHERE LAT_N BETWEEN 38.7880 AND 137.2345;
