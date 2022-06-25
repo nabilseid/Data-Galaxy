05e4ed61e2785a26dbad9e375f335183bb16e990
Nabil Seid <nabeelseid@gmail.com>
2022-06-25 18:07:34 +0300

    Add description for average-population-of-each-continent

--- sql-nebulae/hackerRank-sql-prepare/basic-join/average-population-of-each-continent.sql
+++ sql-nebulae/hackerRank-sql-prepare/basic-join/average-population-of-each-continent.sql
@@ -1,5 +1,45 @@
 /*
-TODO fill description
+Given the CITY and COUNTRY tables, query the names of all the continents 
+(COUNTRY.Continent) and their respective average city populations 
+(CITY.Population) rounded down to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+Input Format
+
+The CITY and COUNTRY tables are described as follows:
+
+CITY
++----------------------------+
+| Field       | Type         |
++----------------------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++----------------------------+
+
+	    COUNTRY 
++-------------------------------+
+| Field          | Type         |
++-------------------------------+
+| CODE           | VARCHAR2(3)  |
+| NAME           | VARCHAR2(44) |
+| CONTINENT      | VARCHAR2(13) |
+| REGION         | VARCHAR2(25) |
+| SURFACEAREA    | NUMBER       |
+| INDEPYEAR      | VARCHAR2(5)  |
+| POPULATAION    | NUMBER       |
+| LIFEEXPECTANCY | VARCHAR2(4)  |
+| GNP            | NUMBER       |
+| GNPOLD         | VARCHAR2(9)  |
+| LOCALNAME      | VARCHAR2(44) |
+| GOVERNMENTFORM | VARCHAR2(44) |
+| HEADOFSTATE    | VARCHAR2(32) |
+| CAPITAL        | VARCHAR2(4)  |
+| CODE2          | VARCHAR2(2)  |
++-------------------------------+
 */
 
 -- solution
